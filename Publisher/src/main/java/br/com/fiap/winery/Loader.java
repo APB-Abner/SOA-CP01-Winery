623fbeeacecb09530a4400423946de3bf26f24d5
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2026-03-12 20:12:46 -0300

    Fix service endpoint URLs in Loader class

--- Publisher/src/main/java/br/com/fiap/winery/Loader.java
+++ Publisher/src/main/java/br/com/fiap/winery/Loader.java
@@ -5,11 +5,11 @@ import jakarta.xml.ws.Endpoint;
 public class Loader {
     public static void main(String[] args) {
         WineStockServiceImplementation wineStock = new WineStockServiceImplementation();
-        Endpoint.publish("http://localhost:8085/WineStockServiceImplementation", wineStock);
+        Endpoint.publish("http://localhost:8085/WineStockService", wineStock);
         System.out.println("Serviço Stock publicado!");
 
-        WineWarningServiceImplementacion wineWarning = new WineWarningServiceImplementacion();
-        Endpoint.publish("http://localhost:8086/WineWarningServiceImplementacion", wineWarning);
+        WineWarningServiceImplementation wineWarning = new WineWarningServiceImplementation();
+        Endpoint.publish("http://localhost:8086/WineWarningService", wineWarning);
         System.out.println("Serviço Warning publicado!");
     }
 }
